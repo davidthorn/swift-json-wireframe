6fc96cac1c5e68f32db93f5dd587e42648a8a7b3
David Thorn <thorn@adesso-mobile.de>
2020-05-01 12:58:44 +0200

    wireframe-datasource: Fixed test

---
+++ WireframeTests/NavigationPlugin/MockNavigationPlugin.swift
@@ -0,0 +1,9 @@
+//
+//  MockNavigationPlugin.swift
+//  WireframeTests
+//
+//  Created by Thorn, David on 01.05.20.
+//  Copyright © 2020 David Thorn. All rights reserved.
+//
+
+import Foundation
--- WireframeTests/NavigationPlugin/NavigationPluginTests.swift
+++ WireframeTests/NavigationPlugin/NavigationPluginTests.swift
@@ -1,15 +1,14 @@
 //
-//  JSONWireframeTests.swift
-//  JSONWireframeTests
+//  NavigationPluginTests.swift
+//  WireframeTests
 //
-//  Created by Thorn, David on 29.04.20.
+//  Created by Thorn, David on 01.05.20.
 //  Copyright © 2020 David Thorn. All rights reserved.
 //
 
 import XCTest
-@testable import JSONWireframe
 
-class JSONWireframeTests: XCTestCase {
+class NavigationPluginTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,12 +18,12 @@ class JSONWireframeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
